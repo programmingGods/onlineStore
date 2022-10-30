fec9a197868cf0735d795d9b39ac720f3ce08c2e
Myslitel666 <tretyakov_02@bk.ru>
2022-10-30 13:34:58 +0500

    product categories

--- index.php
+++ index.php
@@ -13,90 +13,89 @@
     
 <div id = "page-wrap">
 <header>
-    <a href = "index.html" title = "На главную" id = "logo">Sport-Line</a>
-    <span class="contact"><a href="about.html" title="Информация о нас">О нас</a></span>
+    <a href = "index.php" title = "На главную" id = "logo">Sport-Line</a>
+    <span class="contact"><a href="about.php" title="Информация о нас">О нас</a></span>
     <input type = "text" class = "field" placeholder = "Помощь по сайту: "/>
     <span class = "right">
-        <span class="contact"><a href="reg.html" title = "Зарегистрироваться">Регистрация</a></span>
-        <span class = "contact"><a href="auth.html" title="Войти">Вход</a></span>
+        <span class="contact"><a href="reg.php" title = "Зарегистрироваться">Регистрация</a></span>
+        <span class = "contact"><a href="auth.php" title="Войти">Вход</a></span>
     </span>
 </header>
 <div class = "clear"><br/></div>
     <center>
         <div id = "menu">Разделы<hr/></div>
         <div id = "menuHrefs">
-            <a href = "about.html">О нас</a>
-            <a href = "feedback.html">Обратная связь</a>
-            <a href = "auth.html">Вход</a>
-            <a href = "reg.html">Регистрация</a>
+            <a href = "about.php">О нас</a>
+            <a href = "feedback.php">Обратная связь</a>
+            <a href = "auth.php">Вход</a>
+            <a href = "reg.php">Регистрация</a>
         </div>
     </center>
     <div id="wrapper">
         <div id="popular">
             <h2>Популярное</h2>
             <article>
-                <img src="img/jacket_for_boys.jpg" alt="Куртка утеплённая для мальчиков Glissade" title="Куртка утеплённая для мальчиков Glissade" />
+                <img src="img/istockphoto-1350420536-612x612.jpg" alt="Куртка утеплённая для мальчиков Glissade" title="Куртка утеплённая для мальчиков Glissade" />
                 <h3>5199 &#8381</h3>
-                <p>Куртка утеплённая для мальчиков Glissade
-                </p>
+                <p>Куртка утеплённая для мальчиков Glissade</p>
                 <a href"article" title="Добавить в корзину">В корзину</a>
             </article>
             <article>
-                <img src="img/warm_boots_for_men.jpg" alt="Ботинки утепленные мужские Northland Reisen Mid Ltr" title="Ботинки утепленные мужские Northland Reisen Mid Ltr" />
+                <img src="img/istockphoto-885967704-612x612.jpg" alt="Ботинки утепленные мужские Northland Reisen Mid Ltr" title="Ботинки утепленные мужские Northland Reisen Mid Ltr" />
                 <h3>8599 &#8381</h3>
                 <p>Ботинки мужские Northland Reisen Mid Ltr</p>
                 <a href"article" title="Добавить в корзину">В корзину</a>
             </article>
             <article>
-                <img src="img/jacket_for_men.jpg" alt="Куртка утепленная мужская Protest" title="Куртка утепленная мужская Protest" />
+                <img src="img/istockphoto-498658044-612x612.jpg" alt="Куртка утепленная мужская Protest" title="Куртка утепленная мужская Protest" />
                 <h3>11999 &#8381</h3>
                 <p>Куртка утепленная мужская Protest</p>
                 <a href"article" title="Добавить в корзину">В корзину</a>
             </article>
             <article>
-                <img src="img/jacket_for_women.jpg" alt="Куртка утепленная женская Termit" title="Куртка утепленная женская Termit" />
+                <img src="img/istockphoto-1364031095-612x612.jpg" alt="Куртка утепленная женская Termit" title="Куртка утепленная женская Termit" />
                 <h3>4999 &#8381</h3>
                 <p>Куртка утепленная женская Termit</p>
                 <a href"article" title="Добавить в корзину">В корзину</a>
             </article>
         </div>
         <div calss="clear">
-        <div id="main_soc_block">
-            <center>
-            <div id="main_soc_block_in">
-                <h3>Регистрируйтесь на сайте</h3>
-                <p>Только у нас лучшие спортивные товары!!!</p>
-                <a href="reg.html" title="Зарегистрироваться">Регистрируйтесь</a>
+            <div id="main_soc_block">
+                <center>
+                    <div id="main_soc_block_in">
+                    <h3>Регистрируйтесь на сайте</h3>
+                    <p>Только у нас лучшие спортивные товары!!!</p>
+                    <a href="reg.html" title="Зарегистрироваться">Регистрируйтесь</a>
+                    </div>
+                </center>
             </div>
-            </center>
-        </div>
         </div>
         <div id="productCategory">
             <h2>Категории</h2>
             <article>
                 <img src="img/sportswear.png" alt="Одежда" title="Одежда" />
                 <h3>Одежда</h3>
-                <a href"article" title="Смотреть далее">Смотреть далее</a>
+                <a href= "views/categories/clothes.php" title="Смотреть далее">Смотреть далее</a>
             </article>
             <article>
                 <img src="img/shoes.png" alt="Обувь" title="Обувь" />
                 <h3>Обувь</h3>
-                <a href"article" title="Смотреть далее">Смотреть далее</a>
+                <a href = "views/categories/shoes.php" title="Смотреть далее">Смотреть далее</a>
             </article>
             <article>
-                <img src="img/uniforms.png" alt="Экипировака" title="Экипировака" />
-                <h3>Экипировака</h3>
-                <a href"article" title="Смотреть далее">Смотреть далее</a>
+                <img src="img/uniforms.png" alt="Экипировка" title="Экипировка" />
+                <h3>Экипировка</h3>
+                <a href = "views/categories/equipment.php" title="Смотреть далее">Смотреть далее</a>
             </article>
             <article id = "leftArticle">
                 <img src="img/equipment.png" alt="Снаряды" title="Снаряды" />
                 <h3>Снаряды</h3>
-                <a href"article" title="Смотреть далее">Смотреть далее</a>
+                <a href = "views/categories/missiles.php" title="Смотреть далее">Смотреть далее</a>
             </article>
             <article>
                 <img src="img/nutrition.png" alt="Питание" title="Питание" />
                 <h3>Питание</h3>
-                <a href"article" title="Смотреть далее">Смотреть далее</a>
+                <a href = "views/categories/nutrition.php" title="Смотреть далее">Смотреть далее</a>
             </article>
         </div>
     </div>
